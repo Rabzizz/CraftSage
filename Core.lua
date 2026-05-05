a40da9519fd54deba6fbb11a64f76ec79dee7d41
Rabzizz <amiral98.jeux@gmail.com>
2026-05-05 19:01:15 +0200

    feat: addon scaffold with TOC and stubs

---
+++ Core.lua
@@ -0,0 +1,14 @@
+local AddonName, NS = ...
+
+local CraftSage = LibStub("AceAddon-3.0"):NewAddon("CraftSage",
+  "AceEvent-3.0",
+  "AceConsole-3.0",
+  "AceHook-3.0"
+)
+NS.CraftSage = CraftSage
+
+function CraftSage:OnInitialize()
+end
+
+function CraftSage:OnEnable()
+end
---
+++ Data/Engineering.lua
@@ -0,0 +1,1 @@
+CraftSageData = CraftSageData or {}

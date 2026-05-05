3e7079f980c9c7e2bee68efe6267e7285afb1e54
Rabzizz <amiral98.jeux@gmail.com>
2026-05-05 19:18:16 +0200

    feat: add Alchemy and Engineering leveling data

--- Data/Alchemy.lua
+++ Data/Alchemy.lua
@@ -1,1 +1,87 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Alchemy"] = {
+  hc_recommended = true,
+  steps = {
+    {
+      recipe      = "Minor Healing Potion",
+      skill_up_to = 55,
+      qty         = 65,
+      mats = {
+        { item = "Peacebloom",  count = 1 },
+        { item = "Silverleaf",  count = 1 },
+        { item = "Empty Vial",  count = 1 },
+      },
+    },
+    {
+      recipe      = "Lesser Healing Potion",
+      skill_up_to = 110,
+      qty         = 70,
+      mats = {
+        { item = "Briarthorn",  count = 1 },
+        { item = "Silverleaf",  count = 1 },
+        { item = "Leaded Vial", count = 1 },
+      },
+    },
+    {
+      recipe      = "Elixir of Wisdom",
+      skill_up_to = 140,
+      qty         = 40,
+      mats = {
+        { item = "Briarthorn",   count = 1 },
+        { item = "Swiftthistle", count = 1 },
+        { item = "Leaded Vial",  count = 1 },
+      },
+    },
+    {
+      recipe      = "Healing Potion",
+      skill_up_to = 185,
+      qty         = 55,
+      mats = {
+        { item = "Bruiseweed",  count = 1 },
+        { item = "Briarthorn",  count = 1 },
+        { item = "Leaded Vial", count = 1 },
+      },
+    },
+    {
+      recipe      = "Greater Healing Potion",
+      skill_up_to = 215,
+      qty         = 40,
+      mats = {
+        { item = "Kingsblood",   count = 1 },
+        { item = "Liferoot",     count = 1 },
+        { item = "Crystal Vial", count = 1 },
+      },
+    },
+    {
+      recipe      = "Superior Healing Potion",
+      skill_up_to = 250,
+      qty         = 50,
+      mats = {
+        { item = "Sungrass",     count = 1 },
+        { item = "Kingsblood",   count = 1 },
+        { item = "Crystal Vial", count = 1 },
+      },
+    },
+    {
+      recipe      = "Superior Mana Potion",
+      skill_up_to = 265,
+      qty         = 25,
+      mats = {
+        { item = "Sungrass",     count = 2 },
+        { item = "Blindweed",    count = 1 },
+        { item = "Crystal Vial", count = 1 },
+      },
+    },
+    {
+      recipe      = "Major Healing Potion",
+      skill_up_to = 300,
+      qty         = 40,
+      mats = {
+        { item = "Golden Sansam",       count = 3 },
+        { item = "Mountain Silversage", count = 1 },
+        { item = "Crystal Vial",        count = 1 },
+      },
+    },
+  },
+}
--- Data/Engineering.lua
+++ Data/Engineering.lua
@@ -1,1 +1,106 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Engineering"] = {
+  hc_recommended = true,
+  steps = {
+    {
+      recipe      = "Rough Blasting Powder",
+      skill_up_to = 50,
+      qty         = 60,
+      mats = {
+        { item = "Rough Stone", count = 1 },
+      },
+    },
+    {
+      recipe      = "Handful of Copper Bolts",
+      skill_up_to = 75,
+      qty         = 35,
+      mats = {
+        { item = "Copper Bar", count = 1 },
+      },
+    },
+    {
+      recipe      = "Copper Tube",
+      skill_up_to = 105,
+      qty         = 40,
+      mats = {
+        { item = "Copper Bar", count = 2 },
+      },
+    },
+    {
+      recipe      = "Coarse Blasting Powder",
+      skill_up_to = 125,
+      qty         = 30,
+      mats = {
+        { item = "Coarse Stone", count = 1 },
+      },
+    },
+    {
+      recipe      = "Bronze Tube",
+      skill_up_to = 150,
+      qty         = 35,
+      mats = {
+        { item = "Bronze Bar", count = 3 },
+        { item = "Weak Flux",  count = 1 },
+      },
+    },
+    {
+      recipe      = "Whirring Bronze Gizmo",
+      skill_up_to = 175,
+      qty         = 35,
+      mats = {
+        { item = "Bronze Bar", count = 2 },
+        { item = "Silk Cloth", count = 1 },
+      },
+    },
+    {
+      recipe      = "Heavy Blasting Powder",
+      skill_up_to = 200,
+      qty         = 35,
+      mats = {
+        { item = "Heavy Stone", count = 1 },
+      },
+    },
+    {
+      recipe      = "Mithril Tube",
+      skill_up_to = 225,
+      qty         = 35,
+      mats = {
+        { item = "Mithril Bar", count = 3 },
+      },
+    },
+    {
+      recipe      = "Mithril Casing",
+      skill_up_to = 250,
+      qty         = 35,
+      mats = {
+        { item = "Mithril Bar", count = 3 },
+      },
+    },
+    {
+      recipe      = "Dense Blasting Powder",
+      skill_up_to = 260,
+      qty         = 20,
+      mats = {
+        { item = "Dense Stone", count = 2 },
+      },
+    },
+    {
+      recipe      = "Thorium Widget",
+      skill_up_to = 285,
+      qty         = 35,
+      mats = {
+        { item = "Thorium Bar", count = 3 },
+        { item = "Runecloth",   count = 1 },
+      },
+    },
+    {
+      recipe      = "Thorium Tube",
+      skill_up_to = 300,
+      qty         = 25,
+      mats = {
+        { item = "Thorium Bar", count = 5 },
+      },
+    },
+  },
+}

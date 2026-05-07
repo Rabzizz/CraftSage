0bac5327bce32e511e3995fda73ca84aeb0fba0f
Rabzizz <amiral98.jeux@gmail.com>
2026-05-07 14:09:12 +0200

    feat: add Enchanting and Cooking leveling data

--- Data/Cooking.lua
+++ Data/Cooking.lua
@@ -1,1 +1,69 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Cooking"] = {
+  hc_recommended = false,
+  steps = {
+    {
+      recipe      = "Spice Bread",
+      skill_up_to = 40,
+      qty         = 55,
+      mats = {
+        { item = "Simple Flour", count = 1 },
+        { item = "Mild Spices",  count = 1 },
+      },
+    },
+    {
+      recipe      = "Roasted Boar Meat",
+      skill_up_to = 85,
+      qty         = 55,
+      mats = {
+        { item = "Raw Boar Meat", count = 1 },
+      },
+    },
+    {
+      recipe      = "Crab Cake",
+      skill_up_to = 130,
+      qty         = 60,
+      mats = {
+        { item = "Crawler Meat",            count = 1 },
+        { item = "Refreshing Spring Water", count = 1 },
+      },
+    },
+    {
+      recipe      = "Curiously Tasty Omelet",
+      skill_up_to = 175,
+      qty         = 65,
+      mats = {
+        { item = "Raptor Egg",  count = 2 },
+        { item = "Mild Spices", count = 1 },
+      },
+    },
+    {
+      recipe      = "Tender Wolf Steak",
+      skill_up_to = 225,
+      qty         = 70,
+      mats = {
+        { item = "Tender Wolf Meat", count = 2 },
+        { item = "Soothing Spices",  count = 1 },
+      },
+    },
+    {
+      recipe      = "Juicy Bear Burger",
+      skill_up_to = 285,
+      qty         = 80,
+      mats = {
+        { item = "Bear Flank",      count = 2 },
+        { item = "Soothing Spices", count = 1 },
+      },
+    },
+    {
+      recipe      = "Smoked Desert Dumplings",
+      skill_up_to = 300,
+      qty         = 25,
+      mats = {
+        { item = "Sandworm Meat",   count = 1 },
+        { item = "Soothing Spices", count = 1 },
+      },
+    },
+  },
+}
--- Data/Enchanting.lua
+++ Data/Enchanting.lua
@@ -1,1 +1,100 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Enchanting"] = {
+  hc_recommended = false,
+  steps = {
+    {
+      recipe      = "Enchant Bracer - Minor Health",
+      skill_up_to = 45,
+      qty         = 55,
+      note        = "Enchant any cheap vendor item (e.g., Worn Shortsword)",
+      mats = {
+        { item = "Strange Dust", count = 1 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Minor Deflection",
+      skill_up_to = 70,
+      qty         = 35,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Strange Dust",          count = 2 },
+        { item = "Lesser Astral Essence", count = 1 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Minor Agility",
+      skill_up_to = 100,
+      qty         = 40,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Lesser Astral Essence", count = 1 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Minor Spirit",
+      skill_up_to = 135,
+      qty         = 50,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Lesser Mystic Essence", count = 2 },
+      },
+    },
+    {
+      recipe      = "Enchant Shield - Lesser Protection",
+      skill_up_to = 155,
+      qty         = 25,
+      note        = "Enchant any cheap shield from vendor",
+      mats = {
+        { item = "Greater Magic Essence", count = 4 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Strength",
+      skill_up_to = 200,
+      qty         = 60,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Greater Mystic Essence", count = 4 },
+      },
+    },
+    {
+      recipe      = "Enchant Cloak - Greater Defense",
+      skill_up_to = 225,
+      qty         = 35,
+      note        = "Enchant any cheap cloak",
+      mats = {
+        { item = "Greater Nether Essence", count = 3 },
+        { item = "Vision Dust",            count = 2 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Greater Strength",
+      skill_up_to = 250,
+      qty         = 35,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Vision Dust", count = 8 },
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Superior Strength",
+      skill_up_to = 290,
+      qty         = 50,
+      note        = "Enchant any cheap vendor item",
+      mats = {
+        { item = "Illusion Dust", count = 8 },
+      },
+    },
+    {
+      recipe      = "Enchant Weapon - Mighty Intellect",
+      skill_up_to = 300,
+      qty         = 15,
+      note        = "Enchant any cheap weapon",
+      mats = {
+        { item = "Large Brilliant Shard", count = 1 },
+        { item = "Illusion Dust",         count = 10 },
+      },
+    },
+  },
+}

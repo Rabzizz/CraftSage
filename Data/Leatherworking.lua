d27907a105e391ace49fb430d65d077c6d768f12
Rabzizz <amiral98.jeux@gmail.com>
2026-05-07 14:06:47 +0200

    feat: add Tailoring and Leatherworking leveling data

--- Data/Leatherworking.lua
+++ Data/Leatherworking.lua
@@ -1,1 +1,98 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Leatherworking"] = {
+  hc_recommended = false,
+  steps = {
+    {
+      recipe      = "Handstitched Leather Belt",
+      skill_up_to = 55,
+      qty         = 65,
+      mats = {
+        { item = "Light Leather", count = 3 },
+        { item = "Coarse Thread", count = 1 },
+      },
+    },
+    {
+      recipe      = "Embossed Leather Gloves",
+      skill_up_to = 100,
+      qty         = 55,
+      mats = {
+        { item = "Light Leather", count = 3 },
+        { item = "Coarse Thread", count = 1 },
+      },
+    },
+    {
+      recipe      = "Fine Leather Belt",
+      skill_up_to = 130,
+      qty         = 40,
+      mats = {
+        { item = "Light Leather", count = 6 },
+        { item = "Fine Thread",   count = 1 },
+      },
+    },
+    {
+      recipe      = "Dark Leather Belt",
+      skill_up_to = 155,
+      qty         = 35,
+      mats = {
+        { item = "Medium Leather",    count = 6 },
+        { item = "Fine Thread",       count = 1 },
+        { item = "Cured Medium Hide", count = 1 },
+      },
+    },
+    {
+      recipe      = "Hillman's Leather Gloves",
+      skill_up_to = 185,
+      qty         = 40,
+      mats = {
+        { item = "Heavy Leather", count = 6 },
+        { item = "Fine Thread",   count = 2 },
+      },
+    },
+    {
+      recipe      = "Guardian Gloves",
+      skill_up_to = 220,
+      qty         = 45,
+      mats = {
+        { item = "Thick Leather", count = 4 },
+        { item = "Silken Thread", count = 2 },
+        { item = "Iron Buckle",   count = 1 },
+      },
+    },
+    {
+      recipe      = "Nightscape Headband",
+      skill_up_to = 250,
+      qty         = 40,
+      mats = {
+        { item = "Thick Leather", count = 4 },
+        { item = "Silken Thread", count = 2 },
+      },
+    },
+    {
+      recipe      = "Nightscape Pants",
+      skill_up_to = 265,
+      qty         = 20,
+      mats = {
+        { item = "Thick Leather", count = 14 },
+        { item = "Silken Thread", count = 2 },
+      },
+    },
+    {
+      recipe      = "Rugged Armor Kit",
+      skill_up_to = 285,
+      qty         = 30,
+      mats = {
+        { item = "Rugged Leather", count = 3 },
+      },
+    },
+    {
+      recipe      = "Wicked Leather Belt",
+      skill_up_to = 300,
+      qty         = 25,
+      mats = {
+        { item = "Rugged Leather", count = 6 },
+        { item = "Rune Thread",    count = 2 },
+      },
+    },
+  },
+}
--- Data/Tailoring.lua
+++ Data/Tailoring.lua
@@ -1,1 +1,103 @@
 CraftSageData = CraftSageData or {}
+
+CraftSageData["Tailoring"] = {
+  hc_recommended = false,
+  steps = {
+    {
+      recipe      = "Bolt of Linen Cloth",
+      skill_up_to = 45,
+      qty         = 55,
+      mats = {
+        { item = "Linen Cloth", count = 2 },
+      },
+    },
+    {
+      recipe      = "Linen Bag",
+      skill_up_to = 70,
+      qty         = 35,
+      mats = {
+        { item = "Bolt of Linen Cloth", count = 3 },
+        { item = "Coarse Thread",       count = 1 },
+      },
+    },
+    {
+      recipe      = "Bolt of Woolen Cloth",
+      skill_up_to = 105,
+      qty         = 50,
+      mats = {
+        { item = "Wool Cloth", count = 3 },
+      },
+    },
+    {
+      recipe      = "Double-stitched Woolen Shoulders",
+      skill_up_to = 150,
+      qty         = 55,
+      mats = {
+        { item = "Bolt of Woolen Cloth", count = 3 },
+        { item = "Fine Thread",          count = 1 },
+      },
+    },
+    {
+      recipe      = "Bolt of Silk Cloth",
+      skill_up_to = 165,
+      qty         = 25,
+      mats = {
+        { item = "Silk Cloth", count = 4 },
+      },
+    },
+    {
+      recipe      = "Azure Silk Belt",
+      skill_up_to = 200,
+      qty         = 45,
+      mats = {
+        { item = "Bolt of Silk Cloth", count = 3 },
+        { item = "Blue Dye",           count = 2 },
+        { item = "Fine Thread",        count = 1 },
+      },
+    },
+    {
+      recipe      = "Bolt of Mageweave",
+      skill_up_to = 215,
+      qty         = 20,
+      mats = {
+        { item = "Mageweave Cloth", count = 4 },
+      },
+    },
+    {
+      recipe      = "Black Mageweave Gloves",
+      skill_up_to = 250,
+      qty         = 50,
+      mats = {
+        { item = "Bolt of Mageweave",   count = 2 },
+        { item = "Heavy Silken Thread", count = 1 },
+      },
+    },
+    {
+      recipe      = "Bolt of Runecloth",
+      skill_up_to = 260,
+      qty         = 20,
+      mats = {
+        { item = "Runecloth", count = 4 },
+      },
+    },
+    {
+      recipe      = "Runecloth Gloves",
+      skill_up_to = 285,
+      qty         = 40,
+      mats = {
+        { item = "Bolt of Runecloth", count = 2 },
+        { item = "Rugged Leather",    count = 1 },
+        { item = "Rune Thread",       count = 1 },
+      },
+    },
+    {
+      recipe      = "Runecloth Headband",
+      skill_up_to = 300,
+      qty         = 25,
+      mats = {
+        { item = "Bolt of Runecloth", count = 4 },
+        { item = "Rune Thread",       count = 2 },
+      },
+    },
+  },
+}

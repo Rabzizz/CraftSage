f83978b909d9072d6ca2666c6f391a4cf6156e20
Rabzizz <amiral98.jeux@gmail.com>
2026-05-08 13:28:48 +0200

    refactor: Enchanting item names to item IDs

--- Data/Enchanting.lua
+++ Data/Enchanting.lua
@@ -4,96 +4,183 @@ CraftSageData["Enchanting"] = {
   hc_recommended = false,
   steps = {
     {
+      recipe      = "Runed Copper Rod",
+      skill_up_to = 2,
+      qty         = 1,
+      mats = {
+        { item = 6217,  count = 1 }, -- Copper Rod
+        { item = 10940, count = 1 }, -- Strange Dust
+        { item = 10938, count = 1 }, -- Lesser Magic Essence
+      },
+    },
+    {
       recipe      = "Enchant Bracer - Minor Health",
-      skill_up_to = 45,
-      qty         = 55,
-      note        = "Enchant any cheap vendor item (e.g., Worn Shortsword)",
+      skill_up_to = 90,
+      qty         = 108,
       mats = {
-        { item = "Strange Dust", count = 1 },
+        { item = 10940, count = 1 }, -- Strange Dust
       },
     },
     {
-      recipe      = "Enchant Bracer - Minor Deflection",
-      skill_up_to = 70,
-      qty         = 35,
-      note        = "Enchant any cheap vendor item",
-      mats = {
-        { item = "Strange Dust",          count = 2 },
-        { item = "Lesser Astral Essence", count = 1 },
-      },
-    },
-    {
-      recipe      = "Enchant Bracer - Minor Agility",
+      recipe      = "Enchant Bracer - Minor Stamina",
       skill_up_to = 100,
-      qty         = 40,
-      note        = "Enchant any cheap vendor item",
+      qty         = 10,
       mats = {
-        { item = "Lesser Astral Essence", count = 1 },
+        { item = 10940, count = 3 }, -- Strange Dust
       },
     },
     {
-      recipe      = "Enchant Bracer - Minor Spirit",
+      recipe      = "Runed Silver Rod",
+      skill_up_to = 101,
+      qty         = 1,
+      mats = {
+        { item = 6338,  count = 1 }, -- Silver Rod
+        { item = 10940, count = 6 }, -- Strange Dust
+        { item = 10939, count = 3 }, -- Greater Magic Essence
+        { item = 1210,  count = 1 }, -- Shadowgem
+      },
+    },
+    {
+      recipe      = "Greater Magic Wand",
+      skill_up_to = 110,
+      qty         = 9,
+      mats = {
+        { item = 4470,  count = 1 }, -- Simple Wood
+        { item = 10939, count = 1 }, -- Greater Magic Essence
+      },
+    },
+    {
+      recipe      = "Enchant Cloak - Minor Agility",
       skill_up_to = 135,
-      qty         = 50,
-      note        = "Enchant any cheap vendor item",
+      qty         = 25,
       mats = {
-        { item = "Lesser Mystic Essence", count = 2 },
+        { item = 10998, count = 1 }, -- Lesser Astral Essence
       },
     },
     {
-      recipe      = "Enchant Shield - Lesser Protection",
+      recipe      = "Enchant Bracer - Lesser Stamina",
       skill_up_to = 155,
-      qty         = 25,
-      note        = "Enchant any cheap shield from vendor",
+      qty         = 20,
       mats = {
-        { item = "Greater Magic Essence", count = 4 },
+        { item = 11083, count = 2 }, -- Soul Dust
+      },
+    },
+    {
+      recipe      = "Runed Golden Rod",
+      skill_up_to = 156,
+      qty         = 1,
+      mats = {
+        { item = 11128, count = 1 }, -- Golden Rod
+        { item = 5500,  count = 1 }, -- Iridescent Pearl
+        { item = 11082, count = 2 }, -- Greater Astral Essence
+        { item = 11083, count = 2 }, -- Soul Dust
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Lesser Strength",
+      skill_up_to = 185,
+      qty         = 40,
+      mats = {
+        { item = 11083, count = 2 }, -- Soul Dust
       },
     },
     {
       recipe      = "Enchant Bracer - Strength",
       skill_up_to = 200,
-      qty         = 60,
-      note        = "Enchant any cheap vendor item",
+      qty         = 15,
       mats = {
-        { item = "Greater Mystic Essence", count = 4 },
+        { item = 11137, count = 1 }, -- Vision Dust
+      },
+    },
+    {
+      recipe      = "Runed Truesilver Rod",
+      skill_up_to = 201,
+      qty         = 1,
+      mats = {
+        { item = 11144, count = 1 }, -- Truesilver Rod
+        { item = 7971,  count = 1 }, -- Black Pearl
+        { item = 11135, count = 2 }, -- Greater Mystic Essence
+        { item = 11137, count = 2 }, -- Vision Dust
+      },
+    },
+    {
+      recipe      = "Enchant Bracer - Strength",
+      skill_up_to = 220,
+      qty         = 25,
+      mats = {
+        { item = 11137, count = 1 }, -- Vision Dust
       },
     },
     {
       recipe      = "Enchant Cloak - Greater Defense",
       skill_up_to = 225,
-      qty         = 35,
-      note        = "Enchant any cheap cloak",
+      qty         = 5,
       mats = {
-        { item = "Greater Nether Essence", count = 3 },
-        { item = "Vision Dust",            count = 2 },
+        { item = 11137, count = 3 }, -- Vision Dust
       },
     },
     {
-      recipe      = "Enchant Bracer - Greater Strength",
+      recipe      = "Enchant Gloves - Agility",
+      skill_up_to = 230,
+      qty         = 5,
+      mats = {
+        { item = 11174, count = 1 }, -- Lesser Nether Essence
+        { item = 11137, count = 1 }, -- Vision Dust
+      },
+    },
+    {
+      recipe      = "Enchant Boots - Stamina",
+      skill_up_to = 235,
+      qty         = 5,
+      mats = {
+        { item = 11137, count = 5 }, -- Vision Dust
+      },
+    },
+    {
+      recipe      = "Enchant Chest - Superior Health",
       skill_up_to = 250,
-      qty         = 35,
-      note        = "Enchant any cheap vendor item",
+      qty         = 25,
       mats = {
-        { item = "Vision Dust", count = 8 },
+        { item = 11137, count = 6 }, -- Vision Dust
       },
     },
     {
-      recipe      = "Enchant Bracer - Superior Strength",
-      skill_up_to = 290,
-      qty         = 50,
-      note        = "Enchant any cheap vendor item",
+      recipe      = "Lesser Mana Oil",
+      skill_up_to = 265,
+      qty         = 20,
       mats = {
-        { item = "Illusion Dust", count = 8 },
+        { item = 11176, count = 3 }, -- Dream Dust
+        { item = 8831,  count = 2 }, -- Purple Lotus
+        { item = 8925,  count = 1 }, -- Crystal Vial
       },
     },
     {
-      recipe      = "Enchant Weapon - Mighty Intellect",
+      recipe      = "Enchant Shield - Greater Stamina",
+      skill_up_to = 294,
+      qty         = 30,
+      mats = {
+        { item = 11176, count = 10 }, -- Dream Dust
+      },
+    },
+    {
+      recipe      = "Runed Arcanite Rod",
+      skill_up_to = 295,
+      qty         = 1,
+      mats = {
+        { item = 16206, count = 1 },  -- Arcanite Rod
+        { item = 13926, count = 1 },  -- Golden Pearl
+        { item = 16204, count = 10 }, -- Illusion Dust
+        { item = 16203, count = 4 },  -- Greater Eternal Essence
+        { item = 14343, count = 4 },  -- Small Brilliant Shard
+        { item = 14344, count = 2 },  -- Large Brilliant Shard
+      },
+    },
+    {
+      recipe      = "Enchant Cloak - Superior Defense",
       skill_up_to = 300,
-      qty         = 15,
-      note        = "Enchant any cheap weapon",
+      qty         = 5,
       mats = {
-        { item = "Large Brilliant Shard", count = 1 },
-        { item = "Illusion Dust",         count = 10 },
+        { item = 16204, count = 8 },  -- Illusion Dust
       },
     },
   },

477e9a8701caacacc5fd4c79773cac501a711b03
Rabzizz <amiral98.jeux@gmail.com>
2026-05-08 13:28:48 +0200

    refactor: Tailoring item names to item IDs

--- Data/Tailoring.lua
+++ Data/Tailoring.lua
@@ -6,97 +6,173 @@ CraftSageData["Tailoring"] = {
     {
       recipe      = "Bolt of Linen Cloth",
       skill_up_to = 45,
-      qty         = 55,
+      qty         = 95,
       mats = {
-        { item = "Linen Cloth", count = 2 },
+        { item = 2589, count = 2 },  -- Linen Cloth
       },
     },
     {
-      recipe      = "Linen Bag",
+      recipe      = "Linen Belt",
       skill_up_to = 70,
-      qty         = 35,
+      qty         = 25,
       mats = {
-        { item = "Bolt of Linen Cloth", count = 3 },
-        { item = "Coarse Thread",       count = 1 },
+        { item = 2996, count = 1 },  -- Bolt of Linen Cloth
+        { item = 2320, count = 1 },  -- Coarse Thread
+      },
+    },
+    {
+      recipe      = "Reinforced Linen Cape",
+      skill_up_to = 75,
+      qty         = 5,
+      mats = {
+        { item = 2996, count = 2 },  -- Bolt of Linen Cloth
+        { item = 2320, count = 3 },  -- Coarse Thread
       },
     },
     {
       recipe      = "Bolt of Woolen Cloth",
-      skill_up_to = 105,
-      qty         = 50,
+      skill_up_to = 100,
+      qty         = 45,
       mats = {
-        { item = "Wool Cloth", count = 3 },
+        { item = 2592, count = 3 },  -- Wool Cloth
+      },
+    },
+    {
+      recipe      = "Simple Kilt",
+      skill_up_to = 110,
+      qty         = 15,
+      mats = {
+        { item = 2996, count = 4 },  -- Bolt of Linen Cloth
+        { item = 2321, count = 1 },  -- Fine Thread
       },
     },
     {
       recipe      = "Double-stitched Woolen Shoulders",
-      skill_up_to = 150,
-      qty         = 55,
+      skill_up_to = 125,
+      qty         = 15,
       mats = {
-        { item = "Bolt of Woolen Cloth", count = 3 },
-        { item = "Fine Thread",          count = 1 },
+        { item = 2997, count = 3 },  -- Bolt of Woolen Cloth
+        { item = 2321, count = 2 },  -- Fine Thread
       },
     },
     {
       recipe      = "Bolt of Silk Cloth",
-      skill_up_to = 165,
-      qty         = 25,
+      skill_up_to = 145,
+      qty         = 205,
       mats = {
-        { item = "Silk Cloth", count = 4 },
+        { item = 4306, count = 4 },  -- Silk Cloth
       },
     },
     {
-      recipe      = "Azure Silk Belt",
-      skill_up_to = 200,
-      qty         = 45,
+      recipe      = "Azure Silk Hood",
+      skill_up_to = 160,
+      qty         = 20,
       mats = {
-        { item = "Bolt of Silk Cloth", count = 3 },
-        { item = "Blue Dye",           count = 2 },
-        { item = "Fine Thread",        count = 1 },
+        { item = 4305, count = 2 },  -- Bolt of Silk Cloth
+        { item = 2321, count = 1 },  -- Fine Thread
+        { item = 6260, count = 2 },  -- Blue Dye
+      },
+    },
+    {
+      recipe      = "Silk Headband",
+      skill_up_to = 170,
+      qty         = 10,
+      mats = {
+        { item = 4305, count = 3 },  -- Bolt of Silk Cloth
+        { item = 2321, count = 2 },  -- Fine Thread
+      },
+    },
+    {
+      recipe      = "Formal White Shirt",
+      skill_up_to = 175,
+      qty         = 5,
+      mats = {
+        { item = 4305, count = 3 },  -- Bolt of Silk Cloth
+        { item = 2321, count = 1 },  -- Fine Thread
+        { item = 2324, count = 2 },  -- Bleach
       },
     },
     {
       recipe      = "Bolt of Mageweave",
-      skill_up_to = 215,
+      skill_up_to = 185,
+      qty         = 100,
+      mats = {
+        { item = 4338, count = 5 },  -- Mageweave Cloth
+      },
+    },
+    {
+      recipe      = "Crimson Silk Vest",
+      skill_up_to = 205,
       qty         = 20,
       mats = {
-        { item = "Mageweave Cloth", count = 4 },
+        { item = 4305, count = 4 },  -- Bolt of Silk Cloth
+        { item = 2321, count = 2 },  -- Fine Thread
+        { item = 2604, count = 2 },  -- Red Dye
+      },
+    },
+    {
+      recipe      = "Crimson Silk Pantaloons",
+      skill_up_to = 215,
+      qty         = 10,
+      mats = {
+        { item = 4305, count = 4 },  -- Bolt of Silk Cloth
+        { item = 4291, count = 2 },  -- Silken Thread
+        { item = 2604, count = 2 },  -- Red Dye
+      },
+    },
+    {
+      recipe      = "Orange Mageweave Shirt",
+      skill_up_to = 220,
+      qty         = 5,
+      mats = {
+        { item = 4339, count = 1 },  -- Bolt of Mageweave
+        { item = 8343, count = 1 },  -- Heavy Silken Thread
+        { item = 6261, count = 1 },  -- Orange Dye
       },
     },
     {
       recipe      = "Black Mageweave Gloves",
-      skill_up_to = 250,
-      qty         = 50,
+      skill_up_to = 230,
+      qty         = 10,
       mats = {
-        { item = "Bolt of Mageweave",   count = 2 },
-        { item = "Heavy Silken Thread", count = 1 },
+        { item = 4339, count = 2 },  -- Bolt of Mageweave
+        { item = 8343, count = 2 },  -- Heavy Silken Thread
+      },
+    },
+    {
+      recipe      = "Black Mageweave Headband",
+      skill_up_to = 250,
+      qty         = 25,
+      mats = {
+        { item = 4339,  count = 3 }, -- Bolt of Mageweave
+        { item = 8343,  count = 2 }, -- Heavy Silken Thread
       },
     },
     {
       recipe      = "Bolt of Runecloth",
       skill_up_to = 260,
-      qty         = 20,
+      qty         = 155,
       mats = {
-        { item = "Runecloth", count = 4 },
+        { item = 14047, count = 5 }, -- Runecloth
+      },
+    },
+    {
+      recipe      = "Runecloth Belt",
+      skill_up_to = 280,
+      qty         = 25,
+      mats = {
+        { item = 14048, count = 3 }, -- Bolt of Runecloth
+        { item = 14341, count = 1 }, -- Rune Thread
       },
     },
     {
       recipe      = "Runecloth Gloves",
-      skill_up_to = 285,
-      qty         = 40,
-      mats = {
-        { item = "Bolt of Runecloth", count = 2 },
-        { item = "Rugged Leather",    count = 1 },
-        { item = "Rune Thread",       count = 1 },
-      },
-    },
-    {
-      recipe      = "Runecloth Headband",
       skill_up_to = 300,
-      qty         = 25,
+      qty         = 20,
       mats = {
-        { item = "Bolt of Runecloth", count = 4 },
-        { item = "Rune Thread",       count = 2 },
+        { item = 14048, count = 4 }, -- Bolt of Runecloth
+        { item = 8170,  count = 4 }, -- Rugged Leather
+        { item = 14341, count = 1 }, -- Rune Thread
       },
     },
   },

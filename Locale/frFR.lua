5acd468603c7a9d50bb8cc51f38edb43f31070d1
Rabzizz <amiral98.jeux@gmail.com>
2026-05-19 21:22:39 +0200

    feat: add ALT_TRACKER locale keys to enUS, frFR, deDE

--- Locale/frFR.lua
+++ Locale/frFR.lua
@@ -29,3 +29,7 @@ L["THEME_DEFAULT"]      = "Default"
 L["THEME_BLIZZARD"]     = "Blizzard Classic"
 L["THEME_DARK"]         = "Dark"
 L["THEME_ARCANE"]       = "Arcane"
+
+L["ALT_TRACKER_TITLE"] = "CraftSage \226\128\148 Alts"
+L["ALT_TRACKER_EMPTY"] = "No characters tracked yet.\nOpen a profession window to begin."
+L["ALT_TRACKER_YOU"]   = "you"
